ed0e6f48664423b833904fc100ec434d6799d82c
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-24 19:18:04 +0900

    セッション機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :users, only: [:show, :new, :create, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :pictures do
     collection do
       post :confirm
