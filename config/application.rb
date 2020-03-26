7ff0ca1c8c453527d9bb27c8d76de3b76efccf11
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-26 13:42:06 +0900

    リソース追加、文言を変更

--- config/application.rb
+++ config/application.rb
@@ -8,18 +8,11 @@ Bundler.require(*Rails.groups)
 
 module InstagramClone
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-  end
-end
-
-module PairsLike
-  class Application < Rails::Application
     config.generators do |g|
       g.javascripts false
       g.helper false
